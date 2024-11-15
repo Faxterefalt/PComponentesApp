5caf93478b73969dfb47462f9bcf684f4daa6219
Faxterefalt <azugafabian321@gmail.com>
2024-11-15 15:22:12 -0400

    pantallas de Javier

--- App.tsx
+++ App.tsx
@@ -3,20 +3,25 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { enableScreens } from 'react-native-screens';
 import { View, StyleSheet } from 'react-native';
+
 import SearchBar from './componentes/SearchBar';
 import Categories from './componentes/Categories';
 import ContentSection from './componentes/ContentSection';
 import NavigationBar from './componentes/NavigationBar';
 import BuildPage from './componentes/pages/BuildPage';
-//import UserPage from './componentes/pages/UserPage';
 import BuildPageList from './componentes/pages/BuildPageList';
 
-// pantallas nativas
+// Importaciones nuevas
+import LoginScreen from './src/screens/LoginScreen';
+import RegisterScreen from './src/screens/RegisterScreen';
+import HomeScreen from './src/screens/HomeScreen';
+
+// Habilitar pantallas nativas
 enableScreens();
 
 const Stack = createStackNavigator();
 
-const HomeScreen = () => {
+const HomePage = () => {
   return (
     <View style={styles.container}>
       <SearchBar />
@@ -40,11 +45,16 @@ const nuevoProducts = [
 const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator screenOptions={{ headerShown: false }}>
+      <Stack.Navigator screenOptions={{ headerShown: false }} initialRouteName="Login">
+        {/* Rutas de login y registro */}
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Register" component={RegisterScreen} />
+
+        {/* Ruta para Home y demás vistas */}
         <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="HomePage" component={HomePage} />
         <Stack.Screen name="BuildPage" component={BuildPage} />
         <Stack.Screen name="BuildPageList" component={BuildPageList} />
-        
       </Stack.Navigator>
     </NavigationContainer>
   );
